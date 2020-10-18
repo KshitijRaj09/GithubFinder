b837acdf4bcf18e1acfce2de058cf61e483a5e4b
Kshitij Raj <peacekshitij@gmail.com>
2020-10-18 22:57:20 +0530

    Updates

---
+++ precache-manifest.915934e2c161196d03ddfb479c4577be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7efcc056238519f2ac2e8ac535a5320",
+    "url": "/GithubFinder/index.html"
+  },
+  {
+    "revision": "f9c7a15111beb9b224a4",
+    "url": "/GithubFinder/static/css/main.faef0c8f.chunk.css"
+  },
+  {
+    "revision": "c2fd9283582a66a7ca70",
+    "url": "/GithubFinder/static/js/2.292ea6eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GithubFinder/static/js/2.292ea6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c7a15111beb9b224a4",
+    "url": "/GithubFinder/static/js/main.0cbbaf37.chunk.js"
+  },
+  {
+    "revision": "400fda76c7232a632f7e",
+    "url": "/GithubFinder/static/js/runtime-main.1e47d790.js"
+  }
+]);
